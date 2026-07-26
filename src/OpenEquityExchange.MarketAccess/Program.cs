e5edc5b7a12e36dc648bb78ed0e2d4c78bcc783d
binhld <8991nofear@gmail.com>
2026-07-26 11:38:41 +0700

    feat: add FIX Gateway implementation with configuration files

---
+++ src/OpenEquityExchange.MarketAccess/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OEE.MarketAcess.Extensions;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddLogging(cfg => cfg.AddConsole());
+
+builder.Services.AddFixGateway();
+
+using var host = builder.Build();
+
+await host.RunAsync();
